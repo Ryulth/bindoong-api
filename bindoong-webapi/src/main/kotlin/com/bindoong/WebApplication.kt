ba83ed9a8ead8f6f5cc96226990b8410e4857142
Ryulth <dev.ryulth@gmail.com>
2021-08-21 00:13:11 +0900

    Add default timezone

--- bindoong-webapi/src/main/kotlin/com/bindoong/WebApplication.kt
+++ bindoong-webapi/src/main/kotlin/com/bindoong/WebApplication.kt
@@ -2,9 +2,16 @@ package com.bindoong
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.TimeZone
+import javax.annotation.PostConstruct
 
 @SpringBootApplication
-class Application
+class Application {
+    @PostConstruct
+    fun init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
